29954e2018c774fce105eeed5dd1389f048e1bc7
AnnaPetrova <anna.angova@gmail.com>
2026-06-04 12:47:21 +0300

    feat: create UserDto

---
+++ src/main/java/app/model/dto/user/UserDto.java
@@ -0,0 +1,28 @@
+package app.model.dto.user;
+
+import app.model.dto.budget.BudgetDto;
+import app.model.dto.saving.SavingGoalsDto;
+import app.model.dto.transaction.TransactionDto;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Builder
+@Data
+public class UserDto {
+
+    private UUID id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
+    private List<TransactionDto> transactions;
+    private List<BudgetDto> budgets;
+    private List<SavingGoalsDto> saving;
+}
