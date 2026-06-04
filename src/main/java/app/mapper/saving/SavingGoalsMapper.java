6be17fb2b5b60fd2de3c12d4490eeaaf0e90c656
AnnaPetrova <anna.angova@gmail.com>
2026-06-04 12:43:29 +0300

    feat: create SavingGoalsMapper to convert entity to dto

---
+++ src/main/java/app/mapper/saving/SavingGoalsMapper.java
@@ -0,0 +1,22 @@
+package app.mapper.saving;
+
+import app.model.dto.saving.SavingGoalsDto;
+import app.model.entities.saving.SavingGoal;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class SavingGoalsMapper {
+
+    public static SavingGoalsDto toDto(SavingGoal savingGoal){
+
+
+        if(savingGoal == null){
+            return null;
+        }
+        return SavingGoalsDto.builder()
+                .id(savingGoal.getId())
+                .user(savingGoal.getUser())
+                .goalType(savingGoal.getGoalType())
+                .build();
+    }
+}
