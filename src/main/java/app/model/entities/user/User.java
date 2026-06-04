8e677a1694837c3219a67d1ab7b807070b611dea
AnnaPetrova <anna.angova@gmail.com>
2026-06-04 08:58:30 +0300

    feat: create user entity

---
+++ src/main/java/app/model/entities/user/User.java
@@ -0,0 +1,40 @@
+package app.model.entities.user;
+
+import app.model.entities.budget.Budget;
+import app.model.entities.saving.SavingGoal;
+import app.model.entities.transaction.Transaction;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(nullable = false, unique = true)
+    private String username;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
+    private List<Transaction> transactions;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
+    private List<Budget> budgets;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
+    private List<SavingGoal> saving;
+}
