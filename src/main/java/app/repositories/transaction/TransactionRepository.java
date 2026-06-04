98d2802c6c30ca99dc4ed5ce98be4850be0016ed
AnnaPetrova <anna.angova@gmail.com>
2026-06-04 09:32:36 +0300

    feat: create transaction repository

---
+++ src/main/java/app/repositories/transaction/TransactionRepository.java
@@ -0,0 +1,11 @@
+package app.repositories.transaction;
+
+import app.model.entities.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+}
