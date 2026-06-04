109c08eb635cedb424fce07a5bcccac0dae42e8c
AnnaPetrova <anna.angova@gmail.com>
2026-06-04 09:32:52 +0300

    feat: create category repository

---
+++ src/main/java/app/repositories/category/CategoryRepository.java
@@ -0,0 +1,11 @@
+package app.repositories.category;
+
+import app.model.entities.category.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+}
