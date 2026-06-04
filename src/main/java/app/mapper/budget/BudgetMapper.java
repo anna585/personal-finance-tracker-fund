2915ad56373fafecb6a48f88f199c72ef54a16c1
AnnaPetrova <anna.angova@gmail.com>
2026-06-04 12:42:46 +0300

    feat: create BudgetMapper to convert entity to dto

---
+++ src/main/java/app/mapper/budget/BudgetMapper.java
@@ -0,0 +1,24 @@
+package app.mapper.budget;
+
+import app.model.dto.budget.BudgetDto;
+import app.model.entities.budget.Budget;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class BudgetMapper {
+
+    public static BudgetDto toDto(Budget budget){
+
+        if(budget == null){
+            return null;
+        }
+
+        return BudgetDto.builder()
+                .id(budget.getId())
+                .user(budget.getUser())
+                .monthlyLimit(budget.getMonthlyLimit())
+                .month(budget.getMonth())
+                .year(budget.getYear())
+                .build();
+    }
+}
