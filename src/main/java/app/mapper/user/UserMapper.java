aa771294c4b79671e9b9da01b22616637e64d7f9
AnnaPetrova <anna.angova@gmail.com>
2026-06-04 13:07:30 +0300

    feat: add toUserDto method

--- src/main/java/app/mapper/user/UserMapper.java
+++ src/main/java/app/mapper/user/UserMapper.java
@@ -1,15 +1,27 @@
 package app.mapper.user;
 
+import app.mapper.budget.BudgetMapper;
+import app.mapper.saving.SavingGoalsMapper;
+import app.mapper.transaction.TransactionMapper;
+import app.model.dto.budget.BudgetDto;
+import app.model.dto.saving.SavingGoalsDto;
+import app.model.dto.transaction.TransactionDto;
+import app.model.dto.user.UserDto;
 import app.model.dto.user.UserRegisterRequest;
 import app.model.entities.user.User;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @NoArgsConstructor
 public class UserMapper {
 
     public static User toUserEntity(UserRegisterRequest userRegisterRequest){
+
+        if(userRegisterRequest == null){
+            return null;
+        }
 
         return User.builder()
                 .username(userRegisterRequest.getUsername())
@@ -19,6 +31,41 @@ public class UserMapper {
                 .email(userRegisterRequest.getEmail())
                 .createdOn(LocalDateTime.now())
                 .updatedOn(LocalDateTime.now())
+                .build();
+    }
+
+    public static UserDto toUserDto(User user){
+
+        if(user == null){
+            return null;
+        }
+
+        List<TransactionDto> transactionDtoList = user.getTransactions()
+                .stream()
+                .map(TransactionMapper::toDto)
+                .toList();
+
+        List<BudgetDto> budgetDtoList = user.getBudgets()
+                .stream()
+                .map(BudgetMapper::toDto)
+                .toList();
+
+        List<SavingGoalsDto> savingGoalsDtoList = user.getSaving()
+                .stream()
+                .map(SavingGoalsMapper::toDto)
+                .toList();
+
+        return UserDto.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .createdOn(user.getCreatedOn())
+                .updatedOn(user.getUpdatedOn())
+                .transactions(transactionDtoList)
+                .budgets(budgetDtoList)
+                .saving(savingGoalsDtoList)
                 .build();
     }
 }
