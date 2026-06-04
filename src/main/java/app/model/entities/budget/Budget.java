6e5ef7bdd2d0ec2dc1d6c3ed06e45a459f9e12ef
AnnaPetrova <anna.angova@gmail.com>
2026-06-04 09:06:03 +0300

    feat: create budget entity

---
+++ src/main/java/app/model/entities/budget/Budget.java
@@ -0,0 +1,31 @@
+package app.model.entities.budget;
+
+import app.model.entities.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "budgets")
+public class Budget {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal monthlyLimit;
+    private LocalDateTime month;
+    private LocalDateTime year;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
