edec3cdcbb669ca9b9b7cec4e9a8dfcc26853346
AnnaPetrova <anna.angova@gmail.com>
2026-06-04 13:51:35 +0300

    feat: created userService with method register

---
+++ src/main/java/app/services/user/UserService.java
@@ -0,0 +1,63 @@
+package app.services.user;
+
+import app.mapper.user.UserMapper;
+import app.model.dto.user.UserDto;
+import app.model.dto.user.UserRegisterRequest;
+import app.model.entities.budget.Budget;
+import app.model.entities.saving.SavingGoal;
+import app.model.entities.transaction.Transaction;
+import app.model.entities.user.User;
+import app.repositories.user.UserRepository;
+import app.services.budget.BudgetService;
+import app.services.saving.SavingService;
+import app.services.transaction.TransactionService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final BudgetService budgetService;
+    private final SavingService savingService;
+    private final TransactionService transactionService;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, BudgetService budgetService, SavingService savingService, TransactionService transactionService) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.budgetService = budgetService;
+        this.savingService = savingService;
+        this.transactionService = transactionService;
+    }
+
+    public UserDto register(UserRegisterRequest userRegisterRequest){
+
+        userRepository.findByUsername(userRegisterRequest.getUsername())
+                .ifPresent(user -> {
+                    throw new RuntimeException("User with username already exists!");
+                });
+
+      String encodedPassword = passwordEncoder.encode(userRegisterRequest.getPassword());
+      userRegisterRequest.setPassword(encodedPassword);
+
+        User userEntity = UserMapper.toUserEntity(userRegisterRequest);
+
+        Budget defaultBudget =  budgetService.createDefaultBudget(userEntity);
+        userEntity.setBudgets(List.of(defaultBudget));
+
+        SavingGoal defaultSavingGoal =  savingService.createDefaultSaving(userEntity);
+        userEntity.setSaving(List.of(defaultSavingGoal));
+
+        Transaction defaultTransaction= transactionService.createDefaultTransaction(userEntity);
+        userEntity.setTransactions(List.of(defaultTransaction));
+
+
+
+        userRepository.save(userEntity);
+
+        return UserMapper.toUserDto(userEntity);
+    }
+}
