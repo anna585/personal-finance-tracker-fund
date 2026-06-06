b96cae1cf464878c4f0298bff7ae5eeb5ab526e9
AnnaPetrova <anna.angova@gmail.com>
2026-06-06 08:54:04 +0300

    fix: renaming user.getSaving() to user.getSavingGoals()

--- src/main/java/app/model/dto/user/UserRegisterRequest.java
+++ src/main/java/app/model/dto/user/UserRegisterRequest.java
@@ -1,5 +1,7 @@
 package app.model.dto.user;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,10 +10,15 @@ import lombok.Data;
 public class UserRegisterRequest {
 
 
+    @NotBlank
     private String firstName;
+    @NotBlank
     private String lastName;
+    @Size(min = 8, message = "Username must be least than 8 characters!")
     private String username;
+    @Size(min = 8, message = "Username must be least than 8 characters!")
     private String password;
+    @NotBlank
     private String email;
 
 }
