e17fb2b8037e2b370650143f83940cbf286b8def
AnnaPetrova <anna.angova@gmail.com>
2026-06-18 15:40:48 +0300

    feat: add custom queries for budget statistics

--- src/main/java/app/repositories/budget/BudgetRepository.java
+++ src/main/java/app/repositories/budget/BudgetRepository.java
@@ -1,11 +1,21 @@
 package app.repositories.budget;
 
 import app.model.entities.budget.Budget;
+import app.model.entities.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.Month;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface BudgetRepository extends JpaRepository<Budget, UUID> {
+
+    Optional<Budget> findBudgetByUserAndMonthAndYear (User user,Month month, int year);
+
+    Optional<Budget> findByUserAndMonthAndYear(User user, Month month, int year);
+
+    Optional<Budget> findByUserId(UUID id);
+
 }
