7cb2403b16ae112595a9958e3a653000239f3856
AnnaPetrova <anna.angova@gmail.com>
2026-06-22 15:26:24 +0300

    feat: created findUserById method

--- src/main/java/app/repositories/user/UserRepository.java
+++ src/main/java/app/repositories/user/UserRepository.java
@@ -11,4 +11,6 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByUsername(String username);
+
+    User findUserById(UUID id);
 }
