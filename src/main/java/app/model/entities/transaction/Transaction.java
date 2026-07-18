664d1d9c714f8966e03a2e27e85aab08b3495180
AnnaPetrova <anna.angova@gmail.com>
2026-07-18 21:29:14 +0300

    refactor: change LocalDateTime date to LocalDate dat

--- src/main/java/app/model/entities/transaction/Transaction.java
+++ src/main/java/app/model/entities/transaction/Transaction.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Builder
@@ -26,7 +26,7 @@ public class Transaction {
     private TransactionType type;
     @Enumerated(EnumType.STRING)
     private CategoryType categoryType;
-    private LocalDateTime date;
+    private LocalDate date;
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
