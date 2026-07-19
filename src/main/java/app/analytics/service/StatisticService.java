99a604654214834cbf086ec2fb827752fef7e7a7
AnnaPetrova <anna.angova@gmail.com>
2026-07-19 17:28:43 +0300

    feat: create StatisticService with getAllUsersForStatistic method

---
+++ src/main/java/app/analytics/service/StatisticService.java
@@ -0,0 +1,27 @@
+package app.analytics.service;
+
+import app.analytics.client.TransactionClient;
+import app.analytics.dto.StatisticResponse;
+import app.services.user.UserService;
+import app.web.dto.user.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StatisticService {
+
+    private final UserService userService;
+    private final TransactionClient client;
+
+    @PreAuthorize("hasRole('ADMIN')")
+    public StatisticResponse getAllUsersForStatistic() {
+
+        List<UserDto> userList = userService.getAllUsers();
+
+        return  client.getStatisticForAllUsers(userList);
+    }
+}
