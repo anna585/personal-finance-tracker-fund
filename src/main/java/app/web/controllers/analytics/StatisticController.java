036865c96af403e493e93509791be34a5dd1644a
AnnaPetrova <anna.angova@gmail.com>
2026-07-19 17:28:00 +0300

    feat: create StatisticController with GETStatistics

---
+++ src/main/java/app/web/controllers/analytics/StatisticController.java
@@ -0,0 +1,34 @@
+package app.web.controllers.analytics;
+
+import app.analytics.dto.StatisticResponse;
+import app.analytics.service.StatisticService;
+import app.services.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+@Controller
+@RequestMapping("/admin/statistics")
+@RequiredArgsConstructor
+public class StatisticController {
+
+    private final StatisticService statisticService;
+    private final UserService userService;
+
+
+    @GetMapping
+    public ModelAndView getStatistics(){
+
+        StatisticResponse statisticResponse = statisticService.getAllUsersForStatistic();
+
+
+        return new ModelAndView("statistics")
+                .addObject("statisticResponse", statisticResponse)
+                .addObject("users", userService.getAllUsers());
+    }
+
+}
