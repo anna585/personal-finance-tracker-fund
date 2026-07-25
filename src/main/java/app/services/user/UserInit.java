96ed844a710edbc066d3964a1cfc70f140926c83
AnnaPetrova <anna.angova@gmail.com>
2026-07-25 10:14:02 +0300

    feat: add @Profile("!test")

--- src/main/java/app/services/user/UserInit.java
+++ src/main/java/app/services/user/UserInit.java
@@ -8,12 +8,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Slf4j
 @Component
+@Profile("!test")
 @RequiredArgsConstructor
 public class UserInit implements CommandLineRunner {
 
