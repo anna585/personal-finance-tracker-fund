694fd6d150de9e7b0187592525d180236e7a99c2
AnnaPetrova <anna.angova@gmail.com>
2026-07-26 10:00:38 +0300

    feat: create UserFactoryDto

---
+++ src/test/java/app/unit/user/UserFactoryDto.java
@@ -0,0 +1,37 @@
+package app.unit.user;
+
+import app.model.entities.user.UserRole;
+import app.model.entities.user.User;
+import app.web.dto.user.AuthenticationUserDetails;
+import app.web.dto.user.UserDto;
+import lombok.experimental.UtilityClass;
+
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@UtilityClass
+public class UserFactoryDto {
+
+    public static UserDto getUserDto(){
+
+        return UserDto.builder()
+                .id(UUID.randomUUID())
+                .firstName("Anna")
+                .lastName("Angova")
+                .email("ani@abv.bg")
+                .userRole(UserRole.USER)
+                .updatedOn(LocalDateTime.now())
+                .createdOn(LocalDateTime.now())
+                .build();
+    }
+
+    public static AuthenticationUserDetails getUserAdminDto(){
+
+        return AuthenticationUserDetails.builder()
+                .id(UUID.randomUUID())
+                .username("AdminUser")
+                .role(UserRole.ADMIN)
+                .build();
+    }
+}
