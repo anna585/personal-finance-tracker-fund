886c8df7abd8b20cc2bce401d953ed4272c5fe08
AnnaPetrova <anna.angova@gmail.com>
2026-08-03 17:24:51 +0300

    refactor: rename class

--- src/main/java/app/web/dto/user/UsersDetailLists.java
+++ src/main/java/app/web/dto/user/UsersDetailLists.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Builder
 @Data
-public class UsersDetails {
+public class UsersDetailLists {
 
     private List<UserDto> users;
     private List<TransactionDto> transactions;
